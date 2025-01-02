a107e6c3a51fb892333c56b7e748f31424c8515d
brunobotter <62908563+brunobotter@users.noreply.github.com>
2025-01-02 13:27:54 -0300

    first commmit

---
+++ src/main/java/br/com/bruno_botter/gamble/GambleApplication.java
@@ -0,0 +1,13 @@
+package br.com.bruno_botter.gamble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GambleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GambleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/bruno_botter/gamble/GambleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bruno_botter.gamble;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GambleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
